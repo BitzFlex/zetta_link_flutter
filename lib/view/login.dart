23b592671247226a4d0102536634b650ebab08ac
bhseong <bitzflex@gmail.com>
2023-03-26 13:51:51 +0900

    login 화면 layout

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -18,46 +18,76 @@ class ViewLogin extends StatelessWidget {
     return Scaffold(
               appBar: AppBar(title: const Text('Login')),
               backgroundColor: Colors.white,
-              body: Center(
-                      child: Container(
-                          padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                          width: MediaQuery.of(context).size.width * 0.85,                        
-                          child:
-                                  Column(
-
-                                    children: [
-                                                  ElevatedButton(
-                                                      onPressed: () {
-
-                                                        LoginManager().setLogin(true).then((value) {
-                                                          print('Login Pressed');
-                                                          Navigator.of(context).pushReplacement( 
-                                                            MaterialPageRoute(builder: (context) => ViewList() )
-                                                          );
-                                                        });
-
-                                                        // setLogin(true).then((value) {
-                                                        //   print('Login Pressed');
-                                                        //   Navigator.of(context).pushReplacement( 
-                                                        //     MaterialPageRoute(builder: (context) => ViewList() )
-                                                        //   );
-                                                        // });
+              body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 100,),
 
 
-                                                      }, 
-                                                      child: const Text('Login')
-                                                    ),
-                                                  ElevatedButton(
-                                                      onPressed: () {}, 
-                                                      child: const Text('Loout')
-                                                    ),
-
-                                    ],
-                                  )
-
-                                
-                        )
-                    )
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: SizedBox(
+                    width: 200,
+                    height: 250,
+                    /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                    child: Image.asset('asset/images/flutter-logo.png')),
+              ),
+            ),
+            const Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                //TODO FORGOT PASSWORD SCREEN GOES HERE
+              },
+              child: const Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: OutlinedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => const ViewList()));
+                },
+                child: const Text('Login',style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 130,
+            ),
+            const Text('New User? Create Account')
+          ],
+        ),
+      ),
            );
   }
 }
